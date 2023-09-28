2e8450dd284ee56e56baa12e18e9c83b60c23043
Anmol Chourasiya <anmolchourasiya6gmail.com>
2023-09-29 00:36:00 +0530

    project-3rd-commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Visualizer from './components/visualizer';
+import './App.css';
+
+// Parent Component
+class App extends React.Component {
+	
+    render() { 
+        return (
+            <Visualizer />
+        );
+	}
+}
+ 
+export default App;
